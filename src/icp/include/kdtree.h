b5ac2ce6daf2835c781f1190851ae06a692b8b4c
Kashish Garg <kashishgarg9@gmail.com>
2022-12-08 10:33:16 -0500

    kd tree steps written out

---
+++ src/icp/include/kdtree.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <eigen3/Eigen/Dense>
+
+
+class KDTree {
+  public:
+    KDTree(std::vector<Eigen::Vector2f>) {
+      //perform kdtree construction here
+
+    }
+
+};
