add471e542c78dd77cf82ed4f877f4409da960e4
Kashish Garg <kashishgarg9@gmail.com>
2023-06-18 23:19:03 -0400

    added encoder publishing

--- src/ekf_implementation/src/simulated_encoder.cpp
+++ src/ekf_implementation/src/simulated_encoder.cpp
@@ -3,6 +3,7 @@
 #include <gazebo_msgs/GetLinkState.h>
 #include <stdlib.h>     /* abs */
 #include <iostream>
+#include <ekf_implementation/encoder.h>
 
 class tb_sim_enc {
   public:
@@ -10,11 +11,10 @@ class tb_sim_enc {
       this->prev_left = 0.0;
       this->prev_right = 0.0;
       this->prev_timestamp = ros::Time::now();
-      ROS_INFO("min_range: %f", min_range);
-      ROS_INFO("total_range: %f", total_range);
       ROS_INFO("Starting GetLinkState Service Client...");
       ros::ServiceClient client = nh.serviceClient<gazebo_msgs::GetLinkState>("/gazebo/get_link_state");
 
+      this->encoder_pub = nh.advertise<ekf_implementation::encoder>("encoder_ticks", 1000);
       gazebo_msgs::GetLinkState left_srv;
       gazebo_msgs::GetLinkState right_srv;
 
@@ -23,7 +23,7 @@ class tb_sim_enc {
       left_srv.request.reference_frame = "base_footprint";
       right_srv.request.reference_frame = "base_footprint";
 
-      int frequency_to_pub_enc = 4; 
+      int frequency_to_pub_enc = 10; 
       ros::Rate rate(frequency_to_pub_enc);
 
       while(ros::ok()) {
@@ -34,7 +34,7 @@ class tb_sim_enc {
           
           //rad/s for each wheel
           float left_wheel_ang_vel = (float)left_srv.response.link_state.twist.angular.y;
-          float right_wheel_ang_vel = (float)left_srv.response.link_state.twist.angular.y;
+          float right_wheel_ang_vel = (float)right_srv.response.link_state.twist.angular.y;
 
           ROS_INFO("Left Val: %f", left_wheel_ang_vel);
           ROS_INFO("Right Val: %f", right_wheel_ang_vel);
@@ -54,6 +54,11 @@ class tb_sim_enc {
           ROS_INFO("left encoder: %f ticks", floor(deg_travelled_left)); 
           ROS_INFO("right encoder: %f ticks", floor(deg_travelled_right)); 
 
+          ekf_implementation::encoder encoder_msg;
+          encoder_msg.right_encoder = floor(deg_travelled_right);
+          encoder_msg.left_encoder = floor(deg_travelled_left);
+          encoder_pub.publish(encoder_msg);
+
           this->prev_left = left_wheel_ang_vel;
           this->prev_right = right_wheel_ang_vel;
           this->prev_timestamp = msg_ts;
@@ -68,11 +73,9 @@ class tb_sim_enc {
 
   private:
     ros::NodeHandle nh, pnh;
+    ros::Publisher encoder_pub;
     float prev_left;
     float prev_right;
-    float min_range;
-    float total_range;
-    int encoder_res;
     ros::Time prev_timestamp;
     
 };
